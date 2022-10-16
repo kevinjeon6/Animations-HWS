8e28bac5b48013936f21d9efd11e97241921e3ae
Kevin Mattocks <kevin.t.mattocks@gmail.com>
2022-10-16 19:13:30 -0400

    Initial Commit

---
+++ Animations-HWS-6/Animations_HWS_6App.swift
@@ -0,0 +1,17 @@
+//
+//  Animations_HWS_6App.swift
+//  Animations-HWS-6
+//
+//  Created by Kevin Mattocks on 10/16/22.
+//
+
+import SwiftUI
+
+@main
+struct Animations_HWS_6App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Animations-HWS-6/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  Animations-HWS-6
+//
+//  Created by Kevin Mattocks on 10/16/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("This is Project #6 - Animations of the 100 Days of SwiftUI")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
